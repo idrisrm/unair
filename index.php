cba1a9087c3cc8d8f3e9df428cdd3eac4b53b35a
idrisrm <55520214+idrisrm@users.noreply.github.com>
2022-06-07 12:18:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 
 	 			switch(@$_GET['indek']){
 	  			default:
-	 			 echo"<div align='center'>
+	 			 echo"<div align='center' style='margin-top:100px'>
 				  <img src='img/LOGOUNAIR.jpeg' width='250'>
 				  <img src='img/LOGOVOKASI.png' width='250'>
   					</div>";
